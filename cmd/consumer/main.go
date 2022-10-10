b5047d4559d1de67302f873727140cd8ae467bd1
douglarek <douglarek@gmail.com>
2022-10-10 15:32:05 +0800

    Use consumer function based api

--- cmd/consumer/main.go
+++ cmd/consumer/main.go
@@ -5,7 +5,9 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"os/signal"
 	"strings"
+	"syscall"
 
 	"github.com/confluentinc/confluent-kafka-go/kafka"
 )
@@ -15,6 +17,10 @@ var (
 	topic            = flag.String("topic", "", "kafka topics to consumer, multiple divided by the comma")
 	offset           = flag.String("offset", "latest", "auto.offset.reset")
 	timeout          = flag.Duration("timeout", -1, "consumer timeout")
+	saslProtocol     = flag.String("security.protocol", "sasl_plaintext", "security.protocol")
+	saslMechanism    = flag.String("sasl.mechanism", "PLAIN", "sasl.mechanism")
+	saslUser         = flag.String("sasl.username", "", "sasl.username")
+	saslPass         = flag.String("sasl.password", "", "sasl.password")
 )
 
 func init() {
@@ -29,13 +35,21 @@ func main() {
 		panic("both of bootstrap.servers, topic and offset should be not empty")
 	}
 
-	c, err := kafka.NewConsumer(&kafka.ConfigMap{
-		"bootstrap.servers":  *bootstrapServers,
-		"group.id":           "a-kafka-consumer",
-		"auto.offset.reset":  *offset,
-		"enable.auto.commit": false, // no need to commit
-	})
+	cfg := &kafka.ConfigMap{
+		"bootstrap.servers":        *bootstrapServers,
+		"group.id":                 "a-kafka-consumer",
+		"auto.offset.reset":        *offset,
+		"enable.auto.commit":       false, // no need to commit for stream consuming
+		"enable.auto.offset.store": false,
+	}
+	if *saslUser != "" && *saslPass != "" {
+		_ = cfg.SetKey("security.protocol", *saslProtocol)
+		_ = cfg.SetKey("sasl.mechanism", *saslMechanism)
+		_ = cfg.SetKey("sasl.username", *saslUser)
+		_ = cfg.SetKey("sasl.password", *saslPass)
+	}
 
+	c, err := kafka.NewConsumer(cfg)
 	if err != nil {
 		panic(err)
 	}
@@ -49,12 +63,49 @@ func main() {
 
 	log.Printf("%v have been subscribed and start to consume ...", topics)
 
-	for {
+	sigchan := make(chan os.Signal, 1)
+	signal.Notify(sigchan, syscall.SIGINT, syscall.SIGTERM)
+
+	/*for {
 		m, err := c.ReadMessage(*timeout)
 		if err == nil {
 			log.Printf("topic: %v\tpartition: %d\tkey: %s\tvalue: %s", *m.TopicPartition.Topic, m.TopicPartition.Partition, m.Key, m.Value)
 		} else {
 			log.Printf("consumer error: %v\n", err)
 		}
+	}*/
+
+	run := true
+	for run {
+		select {
+		case sig := <-sigchan:
+			log.Printf("Caught signal %v: terminating\n", sig)
+			run = false
+		default:
+			ev := c.Poll(100)
+			if ev == nil {
+				continue
+			}
+
+			switch e := ev.(type) {
+			case *kafka.Message:
+				log.Printf("%% Message on %s:\nk: %s\nv: %s\n", e.TopicPartition, string(e.Key), string(e.Value))
+				if e.Headers != nil {
+					log.Printf("%% Headers: %v\n", e.Headers)
+				}
+				/*_, err := c.StoreMessage(e)
+				if err != nil {
+					log.Printf("%% Error storing offset after message %s:\n", e.TopicPartition)
+				}*/
+			case kafka.Error:
+				log.Printf("%% Error: %v: %v\n", e.Code(), e)
+				if e.Code() == kafka.ErrAllBrokersDown {
+					run = false
+				}
+			default:
+				log.Printf("Ignored %v\n", e)
+			}
+		}
 	}
+	log.Printf("Closing consumer\n")
 }
